b5fe1dc4be7db55dc9a93a306ae8cd5d1cb778a6
Dzmitry Brashavets <Dzmitry_Brashavets@epam.com>
2016-10-22 01:57:32 +0300

    seems that rest api is working...

--- src/clj/modern_cljs/core.clj
+++ src/clj/modern_cljs/core.clj
@@ -1,4 +1,5 @@
 (ns modern-cljs.core
+  (:use ring.util.response)
   (:require [compojure.core :refer :all]
             [compojure.handler :as handler]
             [ring.middleware.json :refer [wrap-json-response]]
@@ -6,7 +7,8 @@
             [compojure.route :as route]
             [ring.middleware.json :as middleware]
             [modern-cljs.views :as views]
-            [modern-cljs.repository.newsrepository :as newsrepo]))
+            [modern-cljs.repository.newsrepository :as newsrepo]
+            [modern-cljs.model.model :as model]))
 
 ;; defroutes macro defines a function that chains individual route
 ;; functions together. The request map is passed to each function in
@@ -17,10 +19,14 @@
            (GET "/news" [] (views/news-list))
            (GET "/news/:id" [id] (views/browse-news id))
            (GET "/add-news" [] (views/add-news-page))
-           (POST "/news" {body :body} (newsrepo/save-news body))
 
            (route/resources "/") ; to serve static pages saved in resources/public directory
            (route/not-found "Page not found")) ; if page is not found
+
+
+(defroutes api-routes
+           (context "/api" []
+                    (POST "/add-news" {body :body} [body] {:status 200 :body (model/->News (.valAt body "purge_everything") "asdf" "asdf" "asdf" "asdf")})))
 
 (defn wrap-log-request [handler]
   (fn [req] ; return handler function
@@ -30,18 +36,17 @@
 ;; site function creates a handler suitable for a standard website,
 ;; adding a bunch of standard ring middleware to app-route:
 (def app-handler
-  (handler/site
-    (-> app-routes
-        wrap-log-request )))
+  (-> app-routes
+      wrap-log-request))
 
 (def api-handler
   (->
-      (handler/api app-routes)
+      (handler/api api-routes)
       (middleware/wrap-json-body)
       (middleware/wrap-json-response)))
 
 (def handler
   (routes                                                   ;;got it here http://stackoverflow.com/questions/30303256/registering-multiple-handlers-while-running-server
-    app-handler
-    api-handler))
+    api-handler
+    app-routes))
 
